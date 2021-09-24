1340e15e8a4ae34497e1f88870b41e434cf27605
Bjoern Naess <bna@dips.no>
2021-09-24 11:20:40 +0200

    Added pulse and temp - and joined all results

--- src/aqls.ts
+++ src/aqls.ts
@@ -1,4 +1,3 @@
-import Observation from './Utils/Observation';
 const AQL_SYSTOLIC = `SELECT
     a_a/data[at0001]/events[at0006]/time,
     a_a/data[at0001]/events[at0006]/data[at0003]/items[at0004]/value/magnitude,
@@ -15,6 +14,30 @@ COMPOSITION a
 CONTAINS OBSERVATION a_a[openEHR-EHR-OBSERVATION.respiration.v1]
 ORDER BY
 a_a/data[at0001]/events[at0002]/time DESC`;
+
+const AQL_TEMP = `SELECT
+a_a/data[at0002]/events[at0003]/time,
+a_a/data[at0002]/events[at0003]/data[at0001]/items[at0004]/value/magnitude,
+a_a/data[at0002]/events[at0003]/data[at0001]/items[at0004]/value/units
+
+
+
+FROM
+COMPOSITION a
+CONTAINS OBSERVATION a_a[openEHR-EHR-OBSERVATION.body_temperature.v2]
+ORDER BY
+a_a/data[at0002]/events[at0003]/time DESC`;
+
+const AQL_PULSE = `SELECT
+a_a/data[at0002]/events[at0003]/time,
+a_a/data[at0002]/events[at0003]/data[at0001]/items[at0004]/value/magnitude,
+a_a/data[at0002]/events[at0003]/data[at0001]/items[at0004]/value/units
+
+FROM
+COMPOSITION a
+CONTAINS OBSERVATION a_a[openEHR-EHR-OBSERVATION.pulse.v1]
+ORDER BY
+a_a/data[at0002]/events[at0003]/time DESC`;
 
 
 
@@ -33,5 +56,15 @@ export const RESPIRASJON: ClinicalConcept = {
     name: "Respirasjon",
     code: "86290005",
     aql: AQL_RESP
+}
+export const TEMP: ClinicalConcept = {
+    name: "Temperatur",
+    code: "703421000",
+    aql: AQL_TEMP
+}
+export const PULSE: ClinicalConcept = {
+    name: "Puls",
+    code: "8499008",
+    aql: AQL_PULSE
 }
 
